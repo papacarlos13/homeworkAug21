59ef50b943069037ea17168eb2dcc5a9eaddfd48
Carlos Mejia <papacarlos13@gmail.com>
2021-09-09 11:14:00 -0500

    Update notes

--- classActivities/week3/3.1.4-Param.js
+++ classActivities/week3/3.1.4-Param.js
@@ -9,15 +9,15 @@ function functionWith2Params(x = 0, y = 1, ...z) {
     return x + y;
 };
 // invoke the function and pass in two numbers
-functionWith2Params(5, 8);
+// functionWith2Params(5, 8);
 //the rest operator will be an empty array
 
 // invoke the function and pass in more than two numbers
-functionWith2Params(1, 2, 3, 4);
+// functionWith2Params(1, 2, 3, 4);
 // the rest operator will be the array [3,4]
 
 // invoke the function and pass in only one number
-functionWith2Params(9);
+// functionWith2Params(9);
 
 //just for fun
 functionWith2Params(1, 2, 3, 4, 5, 6, 7, 8);
