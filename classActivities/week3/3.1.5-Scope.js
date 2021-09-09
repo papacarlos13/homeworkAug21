3216ba44278c96b5a5f8f3cccf7d74eed9a4e111
Carlos Mejia <papacarlos13@gmail.com>
2021-09-09 11:15:24 -0500

    Update notes

--- classActivities/week3/3.1.5-Scope.js
+++ classActivities/week3/3.1.5-Scope.js
@@ -13,16 +13,17 @@ for (let i = 1; i <= 100; i++) {
 }
 return total;
 }
+// the for loop is the child variable
 // invoke the function and console.log its value
 console.log(addNumbersFrom1To100());
 
 // try to print the variable "total" outside the function
 // ? what do you expect ?
-// console.log("total variable, total");
+// console.log(total);
 // total only exists in the scope of the function
 
 // examine the code below,
-// ? which console.log statement will not print when you invoke the parentAndChildScope function ?
+// ? which console.log statement will not print when you invoke the parentAndChildScope function ? (the second function)
 // function childScope(param) {
 //   const childVariable = "I am in the child's scope";
 //   console.log("Child Scope, Parent Variable:", param);
@@ -32,7 +33,13 @@ console.log(addNumbersFrom1To100());
 // function parentAndChildScope() {
 //   const parentVariable = "I am in the parent's scope";
 //   childScope(parentVariable);
+// invokes earlier function giving it a new parameter
+// will print
+
 //   console.log("Parent Scope, Parent Variable:", parentVariable);
+// will print
+
 //   console.log("Parent Scope, Child Variable:", childVariable);
 // }
 //child variable will return error because it is only defined in previous function's scope
+// parentAndChildScope();
