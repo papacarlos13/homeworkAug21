48c4dc29fddccc596c1f3964319dc1c142191492
Carlos Mejia <papacarlos13@gmail.com>
2021-09-19 21:22:15 -0500

    Update Week 4 homework

--- classActivities&Homework/4-week/week4homework/index.js
+++ classActivities&Homework/4-week/week4homework/index.js
@@ -1,3 +1,8 @@
 document
   .querySelector("#Arch")
-  .addEventListener("dblclick", () => alert("Please don't double-click the Arch."));
+  .addEventListener("dblclick", () =>
+    alert("Please don't double-click the Arch.")
+  );
+
+document.querySelector("footer > h4").style.cssText =
+  "font-weight: 800; font-family: Georgia";
