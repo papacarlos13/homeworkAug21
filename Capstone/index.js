a766ae60e049b257f9367ca20c2eceedaff183a3
Carlos Mejia <papacarlos13@gmail.com>
2021-09-27 18:15:44 -0500

    Add media queries and hamburger to capstone

--- Capstone/index.js
+++ Capstone/index.js
@@ -6,3 +6,7 @@ document
       (event.target.title =
         "The Groundhog is ready to help you forecast the next 6 weeks!")
   );
+
+document.querySelector(".fa-bars").addEventListener("click", () => {
+  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+});
